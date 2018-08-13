aeca992771f98ddd29ff6bc4789644669d4c1ad4
Yago Estevez <inbox@yagoestevez.com>
2018-08-13 19:56:34 +0200

    Add interactive data loading to the menu

--- src/main.js
+++ src/main.js
@@ -21,12 +21,16 @@ Promise.all( API_URLs.map( url =>
   fetch( url ).then( res => res.json( ) )
 ) ).then( data => {
   // Hides preloader.
-  document.getElementById( 'preloader' ).classList.add( 'hidden' ); 
+  document.getElementById( 'preloader' ).classList.add( 'hidden' );
+
   // Builds the Treemap Diagram.
   const treemap = new Treemap( data );
   treemap
-    .makeCanvas().buildTreemap().drawTiles().paintColors()
-    .makeTooltip().handleEvents().makeLegend();
+    .makeCanvas( ).buildTreemap( ).drawTiles( ).paintColors( )
+    .makeTooltip( ).handleEvents( ).and.makeLegend( );
+
+  // Handle UI's click events.
+  handleMenu( treemap );
 } )
 .catch( error => { throw new Error( error ) } );
 
@@ -35,8 +39,8 @@ class Treemap {
 
   constructor ( data ) {
     // Sets up sizes.
-    this.chartWidth   = 1100;
-    this.chartHeight  = 700;
+    this.chartWidth   = 980;
+    this.chartHeight  = 680;
     this.margin       = { top: 10, bottom: 100, left: 10, right: 10 };
     this.canvasWidth  = this.chartWidth  - this.margin.left - this.margin.right;
     this.canvasHeight = this.chartHeight - this.margin.top  - this.margin.bottom;
@@ -46,7 +50,15 @@ class Treemap {
     this.mvSales      = data[1];
     this.vgSales      = data[2];
 
-    this.data         = this.ksPledges;
+    // Selects initial dataset depending on the URL parameter provided.
+    // If there's no parameter, Kickstarter dataset will be selected.
+    const urlParams = new URLSearchParams( window.location.search );
+    if ( urlParams.get( 'show' ) === 'kickstarter' || !urlParams.get( 'show' ) )
+      this.data = this.ksPledges;
+    else if ( urlParams.get( 'show' ) === 'movies' )
+      this.data = this.mvSales;
+    else if ( urlParams.get( 'show' ) === 'videogames' )
+      this.data = this.vgSales;
 
     // Chains methods after instantiating.
     this.and          = this;
@@ -63,8 +75,9 @@ class Treemap {
     return this;
   }
 
+  // Builds treemap's hierarchy.
   buildTreemap ( ) {
-    const treemap = d3.treemap( )
+    this.treemap = d3.treemap( )
       .tile( d3.treemapResquarify )
       .size( [ this.canvasWidth, this.canvasHeight ] )
       .round( true )
@@ -75,19 +88,22 @@ class Treemap {
     .sum( d => d.value )
     .sort( ( a, b ) => b.height - a.height || b.value - a.value );
 
-    this.treeLeaves = treemap( root ).leaves( );
+    this.treeLeaves = this.treemap( root ).leaves( );
 
     return this;
   }
 
+  // Draws treemap's tiles and their text.
   drawTiles ( ) {
     const tiles = this.canvas.selectAll( 'g' )
       .data( this.treeLeaves )
       .enter( )
       .append( 'g' )
-        .attr("transform", d => `translate( ${d.x0}, ${d.y0} )` );
+        .attr( 'class'    , 'tile-group' )
+        .attr( 'transform', d => `translate( ${d.x0}, ${d.y0} )` );
 
-    this.tile = tiles.append( 'rect' )
+    this.tile = tiles.append( 'rect' );
+    this.tile.transition( )
       .attr( 'id'           , ( d, i ) => i )
       .attr( 'class'        , 'tile' )
       .attr( 'data-name'    , d => d.data.name )
@@ -111,6 +127,7 @@ class Treemap {
     return this;
   }
 
+  // Fills the treemap with colors.
   paintColors ( ) {
     this.colorScale = d3.scaleOrdinal( )
       .range( [
@@ -180,7 +197,6 @@ class Treemap {
     this.tile
     .on( 'mouseover', function ( d,i ) {
       const dataValue = d3.select( this ).attr( 'data-value' );
-      console.log( d );
       const tipText = `
         <h4 class="title">
           ${d.data.name}
@@ -206,4 +222,65 @@ class Treemap {
     return this;
   }
 
+  // Update Treemap with new data.
+  updateTreemap( data ) {
+    this.data = data === 'kickstarter'
+      ? this.ksPledges
+      : data === 'movies'
+        ? this.mvSales
+        : this.vgSales;
+
+    this.canvas.remove( );
+    this.canvas = this.chart.append( 'g' )
+      .attr( 'transform', `translate( ${this.margin.left}, ${this.margin.top} )` );
+
+    const root = d3.hierarchy( this.data )
+      .eachBefore( d => { d.data.id = ( d.parent ? d.parent.data.id + '.' : '' ) + d.data.name; } )
+      .sum( d => d.value )
+      .sort( ( a, b ) => b.height - a.height || b.value - a.value );
+
+    this.treeLeaves = this.treemap( root ).leaves( );
+
+    this.drawTiles( ).paintColors( ).makeTooltip( ).handleEvents( ).and.makeLegend( );
+
+    return this;
+  }
+
+}
+
+// Handles click events for the menu buttons.
+function handleMenu( treemap ) {
+  const title           = document.getElementById( 'title' );
+  const desc            = document.getElementById( 'description' );
+  const kickstarterBtn  = document.getElementById( 'kickstarterBtn' );
+  const moviesBtn       = document.getElementById( 'movieBtn' );
+  const videogamesBtn   = document.getElementById( 'videogameBtn' );
+
+  kickstarterBtn.addEventListener( 'click', e => {
+    e.preventDefault( );
+    kickstarterBtn.classList.add( 'active' );
+    moviesBtn.classList.remove( 'active' );
+    videogamesBtn.classList.remove( 'active' );
+    treemap.updateTreemap( 'kickstarter' );
+    title.innerText = 'Kickstarter Pledges';
+    desc.innerText  = 'Top 100 Most Pledged Kickstarter Campaigns Grouped By Category';
+  } );
+  moviesBtn.addEventListener( 'click', e => {
+    e.preventDefault( );
+    kickstarterBtn.classList.remove( 'active' );
+    moviesBtn.classList.add( 'active' );
+    videogamesBtn.classList.remove( 'active' );
+    treemap.updateTreemap( 'movies' );
+    title.innerText = 'Movie Sales';
+    desc.innerText  = 'Top 100 Highest Grossing Movies Grouped By Genre';
+  } );
+  videogamesBtn.addEventListener( 'click', e => {
+    e.preventDefault( );
+    kickstarterBtn.classList.remove( 'active' );
+    moviesBtn.classList.remove( 'active' );
+    videogamesBtn.classList.add( 'active' );
+    treemap.updateTreemap( 'videogames' );
+    title.innerText = 'Video Game Sales';
+    desc.innerText  = 'Top 100 Most Sold Video Games Grouped by Platform';
+  } );
 }
